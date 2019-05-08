2e9f5ab970013b7f80557ef3020263a09b077087
Jan-NiklasBlind <janniklas.blind@gmail.com>
2019-05-08 17:55:23 +0200

    Installation script for the turtlebot2 kobuki
    The Turtlebot is currently not supported in melodic and needs to be
    build per source. Most of it is copied from a install script found here https://github.com/gaunthan/Turtlebot2-On-Melodic/blob/master/install_basic.sh
    Seems to use the kinetic version although there are already git branches
    for melodic. Might be worth to look at.

---
+++ install_turtlebot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd /home/$USER/test_ws/src
+# copied from turtlebot2_to_melodic repository: https://github.com/gaunthan/Turtlebot2-On-Melodic
+### 
+git clone https://github.com/turtlebot/turtlebot.git
+git clone https://github.com/turtlebot/turtlebot_msgs.git
+git clone https://github.com/turtlebot/turtlebot_apps.git
+git clone https://github.com/turtlebot/turtlebot_simulator
+
+git clone https://github.com/yujinrobot/kobuki_msgs.git
+git clone https://github.com/yujinrobot/kobuki.git
+mv kobuki/kobuki_description kobuki/kobuki_node \
+  kobuki/kobuki_keyop kobuki/kobuki_safety_controller \
+  kobuki/kobuki_bumper2pc ./
+rm -rf kobuki
+
+git clone https://github.com/yujinrobot/yujin_ocs.git
+mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers .
+rm -rf yujin_ocs
+
+###
+
+# get laptop_battery_monitor-package
+git clone https://github.com/ros-drivers/linux_peripheral_interfaces.git
+cd linux_peripheral_interface
+rm -rf libsensors_monitor
+rm -rf linux_peripheral_interfaces
+
+cd /home/$USER/test_ws
+catkin build
