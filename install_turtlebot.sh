0f980f984fa71120adc7a7d21f749f8abb175733
Jan-NiklasBlind <janniklas.blind@gmail.com>
2019-05-14 18:07:28 +0200

    Changed most of the git clones to newer versions
    This is sleaker and easier to read.

--- install_turtlebot.sh
+++ install_turtlebot.sh
@@ -5,26 +5,14 @@ mkdir -p turtlebot_ws/src
 cd turtlebot_ws
 catkin config --init --install
 cd src
-# copied from turtlebot2_to_melodic repository: https://github.com/gaunthan/Turtlebot2-On-Melodic
-### 
-git clone https://github.com/turtlebot/turtlebot.git
-git clone https://github.com/turtlebot/turtlebot_msgs.git
-git clone https://github.com/turtlebot/turtlebot_apps.git
-git clone https://github.com/turtlebot/turtlebot_simulator
 
-git clone https://github.com/yujinrobot/kobuki_msgs.git
-git clone https://github.com/yujinrobot/kobuki.git
-mv kobuki/kobuki_description kobuki/kobuki_node \
-  kobuki/kobuki_keyop kobuki/kobuki_safety_controller \
-  kobuki/kobuki_bumper2pc ./
-rm -rf kobuki
-
-git clone https://github.com/yujinrobot/yujin_ocs.git
-mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers .
-rm -rf yujin_ocs
-
-###
-
+# get turlebot packages
+git clone https://github.com/turtlebot/turtlebot.git                               
+git clone https://github.com/turtlebot/turtlebot_apps.git                          
+git clone https://github.com/turtlebot/turtlebot_msgs.git                          
+                                                                                   
+git clone https://github.com/yujinrobot/kobuki.git                                 
+                                                                                   
 # get laptop_battery_monitor-package
 git clone https://github.com/ros-drivers/linux_peripheral_interfaces.git
 cd linux_peripheral_interfaces
@@ -35,3 +23,4 @@ cd /home/$USER/turtlebot_ws
 rosdep update && rosdep install --from-paths src/rc_visard_turtlebot_tutorial
 catkin build
 source install/setup.bash
+chmod +x install/share/rc_visard_turtlebot_tutorial/scripts/*
--- install_turtlebot_docker.sh
+++ install_turtlebot_docker.sh
@@ -1,25 +1,13 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 cd /turtlebot_ws/src
-# copied from turtlebot2_to_melodic repository: https://github.com/gaunthan/Turtlebot2-On-Melodic
-### 
+
+# get turtlebot and kobuki packages
 git clone https://github.com/turtlebot/turtlebot.git
-git clone https://github.com/turtlebot/turtlebot_msgs.git
 git clone https://github.com/turtlebot/turtlebot_apps.git
-git clone https://github.com/turtlebot/turtlebot_simulator
+git clone https://github.com/turtlebot/turtlebot_msgs.git
 
-git clone https://github.com/yujinrobot/kobuki_msgs.git
 git clone https://github.com/yujinrobot/kobuki.git
-mv kobuki/kobuki_description kobuki/kobuki_node \
-  kobuki/kobuki_keyop kobuki/kobuki_safety_controller \
-  kobuki/kobuki_bumper2pc ./
-rm -rf kobuki
-
-git clone https://github.com/yujinrobot/yujin_ocs.git
-mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers .
-rm -rf yujin_ocs
-
-###
 
 # get laptop_battery_monitor-package
 git clone https://github.com/ros-drivers/linux_peripheral_interfaces.git
