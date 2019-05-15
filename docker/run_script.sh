ec63b760263a4d2874429c3321d9e16da87684ad
Jan-NiklasBlind <janniklas.blind@gmail.com>
2019-05-15 17:43:39 +0200

    cleaned up the files

--- docker/run_script.sh
+++ docker/run_script.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-# run docker image with all options                                             
-# docker run --name turtlebot --rm \                    
-# -v $(pwd)/catkin_entrypoint.sh:/catkin_entrypoint:x \                                       
-# --network=host \                                                                
-# test_image
 
 executable=$1
-# param1=$2
-# param2=$3
 
 docker run --rm \
 	--device=/dev/kobuki:/dev/kobuki \
 	--device=/dev/input/js0:/dev/input/js0 \
-	 -v $(pwd)/map:/map:rw \
+	 -v ../map:/map:rw \
 	--network host \
 	turtlebot_image \
 	$executable 
-	# $executable $param1 $param2
-
-	# --volume $(pwd)/catkin_entrypoint.sh:/root:rw  \
-
-	# source catkin_ws/devel/setup.bash \
--- docker/start_rc_visard.sh
+++ docker/start_rc_visard.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# run docker image with all options                                             
-# docker run --name turtlebot --rm \                    
-# -v $(pwd)/catkin_entrypoint.sh:/catkin_entrypoint:x \                                       
-# --network=host \                                                                
-# test_image
-
 if [ "$#" -ne 1 ]; then
 	echo "Usage: $0 <rc_visard serial number"
 	exit 1
@@ -17,8 +11,3 @@ docker run --rm \
 	--network host \
 	turtlebot_image \
 	rosrun rc_visard_driver rc_visard_driver _device:=:$serial_number _autostart_dynamics:=True _autostart_dynamics_with_slam:=True _autostop_dynamics:=True _autopublish_trajectory:=True _enable_tf:=True
-	# $executable $param1 $param2
-
-	# --volume $(pwd)/catkin_entrypoint.sh:/root:rw  \
-
-	# source catkin_ws/devel/setup.bash \
