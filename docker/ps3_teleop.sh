ab49d8ff1ae62848711f51b94cddfbd386638967
Jan-NiklasBlind <janniklas.blind@gmail.com>
2019-05-16 16:49:54 +0200

    Created an additional script for ps3_teleoperation
    Including /dev/input/js0 when no joystick is attached leads to an error.
    Therefore a designated file.

--- docker/ps3_teleop.sh
+++ docker/ps3_teleop.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 
-executable=$1
-
 docker run --rm \
 	--device=/dev/kobuki:/dev/kobuki \
+	--device=/dev/input/js0:/dev/input/js0 \
 	-v $(pwd)/map:/map:rw \
 	--network host \
 	turtlebot_image \
-	$executable 
+	roslaunch turtlebot_teleop ps3_teleop.launch
