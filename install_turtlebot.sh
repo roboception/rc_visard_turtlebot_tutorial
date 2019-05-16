763e41bfa56b852571dda3a8326cccfc16fe6879
Jan-NiklasBlind <janniklas.blind@gmail.com>
2019-05-16 17:30:45 +0200

    Specified branches for source packages
    The branches seem to be no longer updated. Therefore this script should
    always have the same output.

--- install_turtlebot.sh
+++ install_turtlebot.sh
@@ -2,15 +2,15 @@
 
 cd ..
 
-# get turlebot packages
-git clone https://github.com/turtlebot/turtlebot.git                               
-git clone https://github.com/turtlebot/turtlebot_apps.git                          
-git clone https://github.com/turtlebot/turtlebot_msgs.git                          
-                                                                                   
-git clone https://github.com/yujinrobot/kobuki.git                                 
-                                                                                   
+# get turtlebot and kobuki packages
+git clone https://github.com/turtlebot/turtlebot.git -b kinetic
+git clone https://github.com/turtlebot/turtlebot_apps.git -b indigo
+git clone https://github.com/turtlebot/turtlebot_msgs.git -b indigo
+
+git clone https://github.com/yujinrobot/kobuki.git -b kinetic
+
 # get laptop_battery_monitor-package
-git clone https://github.com/ros-drivers/linux_peripheral_interfaces.git
+git clone https://github.com/ros-drivers/linux_peripheral_interfaces.git -b kinetic
 cd linux_peripheral_interfaces
 rm -rf libsensors_monitor
 rm -rf linux_peripheral_interfaces
